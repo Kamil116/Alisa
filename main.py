cfecc269b960e3a203a43694f71f07c1a4ff4932
Камиль <kamil.mirgasimoff@yandex.ru>
2020-05-01 02:03:30 +0300

    w

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ def handle_dialog(res, req):
             res['response']['card']['type'] = 'BigImage'
             res['response']['card']['title'] = 'Этот город я знаю.'
             res['response']['card']['image_id'] = cities['москва']
-            res['response']['text'] = req
+            res['response']['text'] = 'Я угадал!'
         elif sessionStorage[user_id]['level'] == 1 and sessionStorage[user_id]['true'] == 1:
             res['response']['card'] = {}
             res['response']['card']['type'] = 'BigImage'
